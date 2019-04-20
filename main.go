cae57bb4edf19ab73fded211ed9b03d9cfc5afd2
ohatakky <ohatakky@gmail.com>
2019-04-20 22:01:52 +0900

    B

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	var a, b, c int
-	fmt.Scan(&a, &b, &c)
 
-	if (b <= a && b <= c && a >= c) || (b >= a && b >= c && a <= c) {
-		fmt.Println("Yes")
-	} else {
-		fmt.Println("No")
+	var n int
+	fmt.Scan(&n)
+
+	var s string
+	fmt.Scan(&s)
+
+	var k int
+	fmt.Scan(&k)
+
+	ts := string(s[k-1])
+
+	str := make([]string, n)
+
+	for i, v := range s {
+		if string(v) != ts {
+			str[i] = "*"
+		} else {
+			str[i] = string(v)
+		}
 	}
+
+	tstr := strings.Join(str, "")
+
+	fmt.Println(tstr)
 
 }
